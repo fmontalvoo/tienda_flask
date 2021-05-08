93556ca568c2647e3b340f4caa7406d76e39098b
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-08 15:16:58 -0500

    Formulario login listo.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 # Paginas de la aplicacion
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
 # Manejos de Errores
+
+
 def page_not_found(error):
     return render_template('errors/404.html'), 404
 
---
+++ app/static/js/login.js
@@ -0,0 +1,6 @@
+(
+    function () {
+        const body = document.querySelector('body');
+        body.classList.add('text-center');
+    }
+)();
