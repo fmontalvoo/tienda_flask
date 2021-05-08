291d3d5e0cbd2aba68007ae7968560d37f9f0798
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-08 14:24:09 -0500

    Manejo de error 404.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
+
 def init_app(settings):
     app.config.from_object(settings)
+    app.register_error_handler(404, page_not_found)
     return app
